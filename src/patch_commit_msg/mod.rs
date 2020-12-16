3dcbe143ead26d8cc91fc505e5df4eb8401c784e
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2020-12-16 14:21:47 +0100

    ISSUE-5: ignore service commented lines

--- src/patch_commit_msg/mod.rs
+++ src/patch_commit_msg/mod.rs
@@ -1,3 +1,7 @@
+fn is_service_comment_line(line: &str) -> bool {
+    line.starts_with("#")
+}
+
 pub fn patch_commit_msg(commit_msg: &Vec<String>,
                         ticket_number: &Option<String>,
                         ticket_prefix: &Option<String>) -> Vec<String> {
@@ -9,18 +13,33 @@ pub fn patch_commit_msg(commit_msg: &Vec<String>,
         None => {}
         Some(ticket) => {
             let mut found = false;
-            for line in lines.iter() {
-                if line.contains(ticket) {
-                    found = true;
-                    break;
+            let mut first_comment_line = None;
+            for (index, line) in lines.iter().enumerate() {
+                if is_service_comment_line(line) {
+                    if first_comment_line == None {
+                        first_comment_line = Some(index);
+                    }
+                } else {
+                    if line.contains(ticket) {
+                        found = true;
+                        break;
+                    }
                 }
             }
             if !found {
-                lines.push(format!("{}{}",
-                                   ticket_prefix
-                                       .clone()
-                                       .unwrap_or("".to_string()),
-                                   ticket.clone()));
+                let new_line = format!("{}{}",
+                                       ticket_prefix
+                                           .clone()
+                                           .unwrap_or("".to_string()),
+                                       ticket.clone());
+                match first_comment_line {
+                    None => {
+                        lines.push(new_line);
+                    }
+                    Some(index) => {
+                        lines.insert(index, new_line);
+                    }
+                }
             }
         }
     };
@@ -74,5 +93,18 @@ mod tests {
                                       &Some("ISSUE-123".to_string()),
                                       &None);
         assert_eq!(2, result.len());
+    }
+
+    #[test]
+    fn ticket_number_is_found_only_inside_commented_line_add_it_to_the_commit_message() {
+        let result = patch_commit_msg(&vec![
+            "1".to_string(),
+            "# comment ISSUE-123".to_string()],
+                                      &Some("ISSUE-123".to_string()),
+                                      &None);
+        assert_eq!(3, result.len());
+        assert_eq!("1", result.get(0).unwrap());
+        assert_eq!("ISSUE-123", result.get(1).unwrap());
+        assert_eq!("# comment ISSUE-123", result.get(2).unwrap());
     }
 }
