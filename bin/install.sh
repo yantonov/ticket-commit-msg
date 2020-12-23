5b7b8801dd43eee7e51f2c7547cf13e4b9f32769
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2020-12-23 10:39:33 +0100

    more informative messages

--- bin/install.sh
+++ bin/install.sh
@@ -7,20 +7,23 @@ cargo build --release
 
 TARGET="${HOME}/bin/ticket-commit-msg"
 if [ -L "${TARGET}" ]; then
-    echo 'Remove old symlink for the hook'
+    echo "Remove old symlink ${TARGET} for the hook"
     rm "${TARGET}"
 fi
 
-echo 'Create symlink for the hook'
+echo "Create symlink ${TARGET} for the hook"
 ln -s $(pwd)/target/release/ticket-commit-msg ${TARGET}
+
+echo ''
 
 TARGET="${HOME}/bin/ticket-commit-msg-install"
 if [ -L "${TARGET}" ]; then
-    echo 'Remove old symlink for install script'
+    echo "Remove old symlink ${TARGET} for install script"
     rm "${TARGET}"
 fi
 
-echo 'Create symlink for install script'
+echo "Create symlink ${TARGET} for install script"
 ln -s $(pwd)/install ${TARGET}
 
+echo ''
 echo 'Done'
