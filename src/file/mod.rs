169556df2622a960fb3820f6e8ffe94061abea60
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2026-08-17 08:42:09 +0200

    file: use Path::display instead of to_str().unwrap() in error messages

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -2,8 +2,8 @@ use std::fs;
 use std::path::Path;
 
 pub fn read_file(path: &Path) -> Result<Vec<String>, String> {
-    let file_content = fs::read_to_string(path)
-        .map_err(|_| format!("Cannot read file: {}", path.to_str().unwrap()))?;
+    let file_content =
+        fs::read_to_string(path).map_err(|_| format!("Cannot read file: {}", path.display()))?;
     let lines: Vec<String> = file_content.lines().map(|x| x.to_string()).collect();
     Ok(lines)
 }
@@ -11,6 +11,6 @@ pub fn read_file(path: &Path) -> Result<Vec<String>, String> {
 pub fn write_file(path: &Path, lines: &[String]) -> Result<(), String> {
     let eoln = "\n";
     fs::write(path, lines.join(eoln))
-        .map_err(|_| format!("cannot write lines to the file: {}", path.to_str().unwrap()))?;
+        .map_err(|_| format!("cannot write lines to the file: {}", path.display()))?;
     Ok(())
 }
